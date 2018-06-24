a58b3d694057f65ac2d312973c7c7c90f8ffeef8
Stefano <stefanovalentini94@gmail.com>
2018-06-24 21:27:32 +0200

    removed unnecessary import

--- src/main/java/it/univaq/we/internshipTutor/repository/StudentRepository.java
+++ src/main/java/it/univaq/we/internshipTutor/repository/StudentRepository.java
@@ -1,6 +1,5 @@
 package it.univaq.we.internshipTutor.repository;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 import it.univaq.we.internshipTutor.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
