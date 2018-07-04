3d9d58c140597757fd39cdebccd4fb444147bd7d
Stefano <stefanovalentini94@gmail.com>
2018-07-04 11:06:34 +0200

    minor ui fixes + adjusted external files location (requires absolute path)

--- src/main/java/it/univaq/we/internshipTutor/controller/InitController.java
+++ src/main/java/it/univaq/we/internshipTutor/controller/InitController.java
@@ -1,5 +1,6 @@
 package it.univaq.we.internshipTutor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
