7b651114310e347ed0a102c8391276677d179b95
Stefano <stefanovalentini94@gmail.com>
2018-07-14 19:32:14 +0200

    fixed registration (student/company) + added error boxes in reg. forms

--- src/main/java/it/univaq/we/internshipTutor/service/IStudentService.java
+++ src/main/java/it/univaq/we/internshipTutor/service/IStudentService.java
@@ -1,12 +1,15 @@
 package it.univaq.we.internshipTutor.service;
 
 import it.univaq.we.internshipTutor.model.Student;
+import it.univaq.we.internshipTutor.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IStudentService {
+
+    void registerStudent(Student s, User u);
 
     public List<Student> findAll();
 
